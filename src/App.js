3913337582e06eeaf0599c1555cba113e8c59b48
anastasyah86 <nastasyah86@gmail.com>
2021-04-07 14:08:08 +0300

    added save to local storage

--- src/App.js
+++ src/App.js
@@ -16,6 +16,19 @@ class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
   };
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
   gatheredData = data => {
     const newData = data;
